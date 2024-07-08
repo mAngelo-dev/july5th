465f17856664dab6d9f17ae45d1c8089b9ae0fa2
mAngelo-dev <miguel.silva@tecnisys.com.br>
2024-07-08 11:44:15 -0300

    Final

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { Analytics } from "@vercel/analytics/react"
+import Image from 'next/image';
 
 export default function App() {
     return (
         <>
             <Analytics/>
             <main className='flex justify-center items-center min-h-screen flex-col relative'>
-                <h1 className='mb-4 text-7xl'>We are dating, that's it!</h1>
+                <Image src='/pictures/sunctisz.jpg' alt='Loading jpg' width={400} height={300} priority/>
+                <h2 className='mt-4 text-3xl'>sunctisz</h2>
+                <h1 className='mb-4 text-7xl'>We are dating, that&apos;s it!</h1>
             </main>
         </>
     );
